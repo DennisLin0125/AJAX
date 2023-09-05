1cb35822e1d7688f10becba06f0e586bc833a718
Dennis <740125aa@gmail.com>
2023-09-05 22:11:06 +0800

    AJAX-範例

--- server.js
+++ server.js
@@ -8,6 +8,68 @@ app.get('/server', (req,res) => {
     res.send('HELLO AJAX');
 });
 
+// JSON響應
+app.get('/json-server', (req,res) => {
+    // 允許跨域
+    res.setHeader('Access-Control-Allow-Origin','*');
+    // 響應一個json
+    const data = {
+        name: "dennis"
+    }
+    // 對數據做轉換
+    let str = JSON.stringify(data);
+    res.send(str);
+});
+
+app.post('/server', (req,res) => {
+    // 允許跨域
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.send('HELLO AJAX POST');
+});
+
+// 針對IE緩存
+app.get('/ie', (req,res) => {
+    // 允許跨域
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.send('HELLO IE');
+});
+
+// 延時響應
+app.get('/delay', (req,res) => {
+    // 允許跨域
+    res.setHeader('Access-Control-Allow-Origin','*');
+    // 設定3秒後回傳數據
+    setTimeout(()=>{
+        res.send('延時響應');
+    },3000);
+});
+
+// jQuery 服務
+app.get('/jQuery-server', (req,res) => {
+    // 允許跨域
+    res.setHeader('Access-Control-Allow-Origin','*');
+    // 響應一個json
+    const data = {
+        name: "dennis_GET"
+    }
+    // 對數據做轉換
+    let str = JSON.stringify(data);
+    res.send(str);
+});
+
+// jQuery 服務
+app.post('/jQuery-server', (req,res) => {
+    // 允許跨域
+    res.setHeader('Access-Control-Allow-Origin','*');
+    // 響應一個json
+    const data = {
+        name: "dennis_POST"
+    }
+    let str = JSON.stringify(data);
+    res.send(str);
+});
+
+
 app.listen('9000',() => {
     console.log("服務已經啟動,9000監聽中.....");
 });
